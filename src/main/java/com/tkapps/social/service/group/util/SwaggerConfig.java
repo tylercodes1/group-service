dadd9fce75bd7a8ef81e299000ef0c9eeb25e107
tkmoney11 <tylerkim@outlook.com>
2021-03-06 19:51:40 -0800

    Cleaned up SwaggerConfig and updated swaggerUI version

--- src/main/java/com/tkapps/social/service/group/util/SwaggerConfig.java
+++ src/main/java/com/tkapps/social/service/group/util/SwaggerConfig.java
@@ -1,32 +1,17 @@
 package com.tkapps.social.service.group.util;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-    @Value("${swagger.host.url}")
-    private String hostUrl;
-
     @Bean
     public Docket api() {
-        return new Docket (DocumentationType.SWAGGER_2)
-                .host(hostUrl)
-                .groupName("Spring Actuator")
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+        return new Docket (DocumentationType.SWAGGER_2);
     }
 
     // TODO finish after hosting
